61d1c914ca921b2e8b0f028cedc818520e88ea9e
Andrew <contact@andrewpd.com>
2017-04-04 16:59:37 +0800

    blog plus adminJ

---
+++ config/social.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+/**
+ * Created by PhpStorm.
+ * User: lufficc
+ * Date: 2016/8/21
+ * Time: 22:47
+ */
+    'facebook' => [
+        'url'  => 'https://www.facebook.com/profile.php?id=0',
+        'fa'   => 'fa fa-facebook fa-fw',
+    ],
+    'twitter'  => [
+        'url'  => 'https://twitter.com/justastriver',
+        'fa'   => 'fa fa-twitter fa-fw',
+    ],
+    'weibo'    => [
+        'url'  => 'http://weibo.com/207076710/home?topnav=1&wvr=6',
+        'fa'   => 'fa fa-weibo fa-fw',
+    ],
+    'github'  => [
+        'url' =>'https://github.com/justastriver',
+        'fa'   => 'fa fa-github fa-fw',
+    ],
+
+];
