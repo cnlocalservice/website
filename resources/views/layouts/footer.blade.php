794adc2cce3489e5bbee0fb2f74a0bb527051d55
Andrew <contact@andrewpd.com>
2017-04-04 17:40:15 +0800

    add footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,9 +3,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
-                    <span>蜀ICP备16008148号-2</span> |
-                    <span>Powered by <a href="https://github.com/lufficc/Xblog">Xblog</a> </span>
+                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }} 2017</a></span> |
+                    <span>京ICP备14006962号-4</span> |
+                    <span>Powered by <a href="https://github.com/lufficc/Xblog">Andrew's Tech</a> </span>
                 </div>
             </div>
         </div>
